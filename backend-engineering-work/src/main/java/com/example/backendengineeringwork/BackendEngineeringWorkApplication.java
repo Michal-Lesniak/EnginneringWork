6d230fcabaf222788a01ba1cd11016c9abe7625d
Michal-Lesniak <michalles1@interia.pl>
2023-10-13 21:04:20 +0200

    Initial commit

---
+++ backend-engineering-work/src/main/java/com/example/backendengineeringwork/BackendEngineeringWorkApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendengineeringwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendEngineeringWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendEngineeringWorkApplication.class, args);
+	}
+
+}
---
+++ backend-engineering-work/src/test/java/com/example/backendengineeringwork/BackendEngineeringWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendengineeringwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendEngineeringWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
