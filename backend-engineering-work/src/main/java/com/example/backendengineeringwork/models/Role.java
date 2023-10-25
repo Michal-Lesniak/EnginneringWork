2fed4593746b6e4a17f820980fc184cf5fb523f1
Michal Lesniak <michalles1@interia.pl>
2023-10-25 23:47:13 +0200

    Half of security implemented

--- backend-engineering-work/src/main/java/com/example/backendengineeringwork/models/Role.java
+++ backend-engineering-work/src/main/java/com/example/backendengineeringwork/models/Role.java
@@ -1,20 +1,6 @@
 package com.example.backendengineeringwork.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.*;
-
-import javax.validation.constraints.NotBlank;
-
-@Entity
-@Data
-public class Role {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @NotBlank
-    private String name;
+public enum Role {
+    USER,
+    ADMIN
 }
